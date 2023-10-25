5ee4f39867eab1bf7fd3d568887cb7263892587e
shafiqr-kmsw <92511342+shafiqr-kmsw@users.noreply.github.com>
2023-10-25 16:16:04 +0800

    updated

---
+++ yeahhost/pre2023/misc/sp015.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+    body {
+        height: 842px;
+        width: 595px;
+        /* to centre page on screen*/
+        margin-left: auto;
+        margin-right: auto;
+    }
+    </style>
+    <!--<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"> </script>-->
+  </head>
+<body>
+
+<h1 align="center">SP015 Course Page</h1>
+<h4 align="center">By Mr Shafiq R</h4>
+<h3 align="center"> Welcome! Find most of the resources you need here!</h3>
+<table style="width:100%"; border="0px">
+    <tr align="center">
+        <td align="center">
+            <h3 align="center">Click<a href="https://www.youtube.com/channel/UCHBSyKlHAfdJcmrkt6wkk7w"><img src="button/yt_phys.gif" width="50px"></a>for lecture videos! </h3>
+        </td>
+        <!-- <td align="center">
+            <h3> Days to PSPM1 2022/23:</h3>-->
+            <!--Timer Code Starts-->
+            <!-- Display the countdown timer in an element -->
+<!--<p id="demo"></p>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 15, 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
+<!--Timer Code Ends-->
+        </td>
+    </tr>
+</table>
+<table style="width:100% " border= "0px solid black">
+	<tr><td>
+		<table style="width:100% ; border-collapse: collapse" border= "0px solid black">
+			<tr>
+			    <td align="center" style="break-word">
+					<b><a href="admin/absence_declaration.html"> Absence</a></b>
+				</td>
+				<td align="center" style="break-word">
+					<dl>
+						<dt><h4>Tutorials</h4></dt>
+						<dt><a href="notes/maths.pdf"> Maths Notes</a></dt>
+						<dt><a href="tutorials/ps1.pdf"> Maths Prerequisite</a></dt>
+						<dt><a href="notes/sem1.pdf">Lecture Notes (2021/22)</a></dt>
+						<dt><a href="constant.pdf"> Constants & Equations (2021/22)</a></dt>
+						<dt><a href="tutorials/Tutorial_2020.pdf"> Tutorials(2020)</a></dt>
+						<dt><a href="https://shafiqr-kmsw.github.io/phy6_22_23/"> Lecture notes & Tutorials (2022/23)</a></dt>
+						<dt><a href="pastyear/pypapers.html"> Past Year Papers</a></dt>
+					</dl>
+				</td>
+			    <td align="center" style="break-word">
+					<dl>
+						<dt><h4>Labs</h4></dt>
+						<dt><a href="labs/lab_manual.pdf">2020 Lab Manual</a></dt>
+						<dt><a href="labs/lab_manual_2223.pdf">2022 Lab Manual</a></dt>
+						<dt><a href="labs/prelabs.pdf"> Prelabs!</a></dt>
+						<dt><a href="labs/homelabs.pdf"> Home-Based Experiments!(2021/22)</a></dt>
+						<dt><a href="labs/calculator.html"> Dataset Generator</a></dt>
+						<dt><a href="labs/report_checklist.pdf"> Lab Report Checklist</a></dt>
+						<dt><a href="labs/report_format.docx"> Lab Report Template</a></dt>
+						<dt><a href="labs/sample_method.docx"> Sample Methodology</a></dt>
+						<dt><a href="labs/error_prop.pdf"> Error Propagation</a></dt>
+					</dl>
+				</td>
+			</tr>
+			</table>
+	</tr></td>
+		</table>
+
+<tr><td>
+		<table style="width:100% ; border-collapse: collapse" border= "0px solid black">
+			<tr>
+				<td align="center" style="break-word">
+					<dl>
+						<dt><h4>Extras</h4></dt>
+						<dt><a href="notes/sem1_2021_slides.pdf"> 2021 Lecture Slides (92MB)</a></dt>
+					</dl>
+				</td>
+			</tr>
+			</table>
+	</tr></td>
+		</table>
+
+<!--Generating lab report-->
+
+<!--
+<tr>
+		<table style="border-collapse: collapse", border="0",;>
+			<td>Generated date and time: </td>
+			<td>
+			    <?php // Prints the day, date, month, year, time, AM or PM 
+			    echo date("l jS \of F Y h:i:s A");?>
+			</td>
+		</table>
+	</tr>
+-->
+</table>
+<footer align="center"> <h6> Copyright sr@kmsw 2021 </h6> </footer>
+</body>
+</html>
