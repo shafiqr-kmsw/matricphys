c3ff8b05fc2402be811f53bcf8f88ccd6cacf142
shafiqr-kmsw <shafiq.kmsw@gmail.com>
2024-06-30 18:42:57 +0800

    updated

---
+++ phy6_files/sp025.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="icon" href="lgphy6.png">
+    <style>
+    body {
+        height: 842px;
+        width: 595px;
+        /* to centre page on screen*/
+        margin-left: auto;
+        margin-right: auto;
+    }
+    </style>
+    <!--<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"> </script>-->
+  </head>
+<body>
+
+<h1 align="center">SP025 Course Page</h1>
+<h4 align="center">By Mr Shafiq R</h4>
+<h3 align="center"> Welcome! Find most of the resources you need here!</h3>
+<!--<h3 align="center">
+    <iframe src="https://free.timeanddate.com/countdown/i851yfem/n2022/cf100/cm0/cu4/ct0/cs0/ca0/co1/cr0/ss0/cacf00/cpc000/pcfff/tc66c/fn3/fs150/szw320/szh135/tatTime%20left%20to%20PSPM2%202022/tac000/tptTime%20since%20PSPM2%202022%20started%20in/tpc000/iso2022-05-19T00:00:00" allowtransparency="true" frameborder="0" width="320" height="135"></iframe>
+</h3>-->
+<table style="width:100%"; border="0px">
+    <tr align="center">
+        <td align="center">
+            <h3 align="center">Click<a href="https://www.youtube.com/channel/UCHBSyKlHAfdJcmrkt6wkk7w"><img src="button/yt_phys.gif" width="50px"></a>for lecture videos! </h3>
+        </td>
+        <td align="center">
+            <h3 align="center"> <iframe src="https://free.timeanddate.com/countdown/i851yfem/n2022/cf100/cm0/cu4/ct0/cs0/ca0/co1/cr0/ss0/cacf00/cpc000/pcfff/tc66c/fn3/fs150/szw320/szh135/tatTime%20left%20to%20PSPM2%202022/tac000/tptTime%20since%20PSPM2%202022%20started%20in/tpc000/iso2022-05-19T00:00:00" allowtransparency="true" frameborder="0" width="320" height="135"></iframe>
+        </td>
+    </tr>
+</table>
+<table style="width:100% " border= "0px solid black">
+	<tr><td>
+		<table style="width:100% ; border-collapse: collapse" border= "0px solid black">
+			<tr>
+			    <td align="center" style="break-word">
+					<b><a href="admin/absence_declaration.html"> Absence</a></b>
+				</td>
+				<td align="center" style="break-word">
+					<dl>
+						<dt><h4>Tutorials</h4></dt>
+						<dt><a href="presem2.pdf">Pre-Semester Worksheet </a></dt>
+						<dt><a href="notes/em_025.pdf">Lecture Notes 2021 (Electromagnetism)</a></dt>
+						<dt><a href="notes/optics_025.pdf">Lecture Notes 2022 (Optics)</a></dt>
+						<dt><a href="constant2.pdf"> Constants & Equations (2021/22)</a></dt>
+						<dt><a href="tutorials/2020_sem2_tuto.pdf"> Tutorials 2020</a></dt>
+						<dt><a href="tutorials/2020_sem2_soln.pdf"> Tutorials 2020 Solution <?php pwd:solusi2020 ?></a></dt> 
+						<dt><a href=""> Tutorials 2021</a></dt>
+						<dt><a href="pastyear/sp025_pyq.pdf"> Past Year Papers</a></dt>
+						<dt><a href="pastyear/pyq_soln.pdf"> Past Year Papers Solution <?phppwd:"solusipyq"?></a></dt>
+					</dl>
+				</td>
+			    <td align="center" style="break-word">
+					<dl>
+						<dt><h4>Labs</h4></dt>
+						<dt><a href="labs/lab_prep.pdf">Lab Preparation Sample</a></dt>
+						<dt><a href="labs/lab_manual.pdf">2020 Lab Manual</a></dt>
+						<dt><a href="labs/025_prelabs.pdf"> Prelabs!</a></dt>
+						<dt><a href="labs/calculator.html"> Dataset Generator</a></dt>
+						<dt><a href="labs/report_checklist.pdf"> Lab Report Checklist</a></dt>
+						<dt><a href="labs/report_format.docx"> Lab Report Template</a></dt>
+						<dt><a href="labs/exp_PE_data.zip"> Photoelectric Effect Experiment Dataset</a></dt>
+					</dl>
+				</td>
+			</tr>
+			</table>
+	</tr></td>
+		</table>
+
+<tr><td>
+		<table style="width:100% ; border-collapse: collapse" border= "0px solid black">
+			<tr>
+				<td align="center" style="break-word">
+					<dl>
+						<dt><h4>Extras</h4></dt>
+						<dt><a href=""> 2021 Lecture Slides</a></dt>
+					</dl>
+				</td>
+			</tr>
+			</table>
+	</tr></td>
+	<tr><td>
+	    For Future use
+	</td></tr>
+		</table>
+
+<!--Generating lab report-->
+
+<!--
+<tr>
+		<table style="border-collapse: collapse", border="0",;>
+			<td>Generated date and time: </td>
+			<td>
+			    <?php // Prints the day, date, month, year, time, AM or PM 
+			    echo date("l jS \of F Y h:i:s A");?>
+			</td>
+		</table>
+	</tr>
+-->
+</table>
+<footer align="center"> <h6> Copyright sr@kmsw 2021 </h6> </footer>
+</body>
+</html>
